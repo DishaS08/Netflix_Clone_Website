55ff6ab0f9f07657a05cb34636b739af21fa6284
DishaS08 <dishasuryawanshi104@gmail.com>
2025-07-13 21:22:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const faqs = document.querySelectorAll(".faqbox");
+let faqs = document.querySelectorAll(".faqbox");
 
-faqs.forEach(box => {
-  box.addEventListener("click", () => {
-    box.classList.toggle("open");
+faqs.forEach(faq => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+
+    let answer = faq.querySelector(".answer");
+    if (answer) {
+      if (answer.style.display === "block") {
+        answer.style.display = "none";
+      } else {
+        answer.style.display = "block";
+      }
+    }
   });
 });
